db96bf5fef8c0ba6675a1f5c12526df2339c2da9
Emma Cohanim <emmacohanim@gmail.com>
2022-09-29 10:32:33 -0700

    commit tree a0c32ac14b62f004b4ba691f484c55dc8e6bd985

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,24 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    if session[:page_views] <= 3
+      article = Article.find(params[:id])
+      render json: article
+    else
+      unauthorized
+    end
   end
 
   private
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
+  end
+
+  def unauthorized
+    render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
   end
 
 end
